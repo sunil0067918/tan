60400ce8579d79930375842782b9f61b166244f4
sunil <109091779+sunil0067918@users.noreply.github.com>
2022-07-12 09:44:47 +0530

    changes

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["tjori.gumlet.io" , "tjori.gumlet.com"],
+  },
+};
+
+module.exports = nextConfig;
